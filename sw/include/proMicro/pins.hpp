84775471ee7bf37904ed91d38b4261308464cdbe
Pascal Pieper <Accounts@PascalPieper.de>
2020-10-22 22:49:22 +0200

    le buidl

--- sw/include/proMicro/pins.hpp
+++ sw/include/proMicro/pins.hpp
@@ -1,6 +1,9 @@
 static constexpr uint8_t MUTE = 10;
 static constexpr uint8_t POT  = A1;
 static constexpr uint8_t LED  = 5;
+static constexpr uint8_t J0   = 0;
+static constexpr uint8_t J1   = 1;
+static constexpr uint8_t J2   = 2;
 
 static constexpr bool has_serial = true;
 static constexpr bool use_batt_check = true;
