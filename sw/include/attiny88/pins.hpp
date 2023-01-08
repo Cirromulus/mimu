d6e2d32ad58be9e1d74a80c2fe4dda5db30e9a07
Pascal Pieper <accounts@pascalpieper.de>
2023-01-08 15:36:54 +0100

    Fixed pin numbers

--- sw/include/attiny88/pins.hpp
+++ sw/include/attiny88/pins.hpp
@@ -2,9 +2,9 @@
 #include <Arduino.h>
 
 static constexpr uint8_t MUTE_DRIVE = 17;   //PC0
-static constexpr uint8_t BUTTON     = A1;   //PC1
-static constexpr uint8_t LEDR       = 19;   //PC2
-static constexpr uint8_t LEDG       = 20;   //PC3
+static constexpr uint8_t BUTTON     = 16;   //PC7
+static constexpr uint8_t LEDG       = 19;   //PC2
+static constexpr uint8_t LEDR       = 20;   //PC3
 static constexpr uint8_t J0   = 0;
 static constexpr uint8_t J1   = 1;
 static constexpr uint8_t J2   = 2;
