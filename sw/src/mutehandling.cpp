f5f1dec5ec6d5d1b42bef8bf459cb39c1039ae81
Pascal Pieper <accounts@pascalpieper.de>
2023-01-13 10:37:30 +0100

    Fixed digipot value calculation

--- sw/src/mutehandling.cpp
+++ sw/src/mutehandling.cpp
@@ -15,8 +15,8 @@ bool initDigipotUnmuted(){
 }
 
 void setMute(const bool mute) {
-    /*
     // Print calculated values of mute profile
+    /*
     static_print<calculated_mute_profile.mute_ramp_on_delay_per_step_us>();
     static_print<calculated_mute_profile.mute_ramp_off_delay_per_step_us>();
     static_print<calculated_mute_profile.value_when_microphone_on>();
@@ -24,10 +24,13 @@ void setMute(const bool mute) {
     static_print<calculated_mute_profile.extra_drive_opto>();
     */
 
+    static_assert(calculated_mute_profile.value_when_microphone_on > calculated_mute_profile.value_when_microphone_off,
+                  "The loop expects the value of _on to be bigger than _off");
+
     // Assumption: High value -> High resistance -> Low optocoupler -> low dampening -> microphone "on"
     if(mute){
         for(AD5258::SignedValue val = calculated_mute_profile.value_when_microphone_on;
-            val >= calculated_mute_profile.value_when_microphone_off;
+            val != calculated_mute_profile.value_when_microphone_off;
             val--) {
                 digipot.writeWiper(val);
                 delayMicroseconds(calculated_mute_profile.mute_ramp_off_delay_per_step_us);
@@ -38,7 +41,7 @@ void setMute(const bool mute) {
     } else {
         enableExtraOptoDriver(false);
         for(AD5258::SignedValue val = calculated_mute_profile.value_when_microphone_off;
-            val <= calculated_mute_profile.value_when_microphone_on;
+            val != calculated_mute_profile.value_when_microphone_on;
             val++) {
                 digipot.writeWiper(val);
                 delayMicroseconds(calculated_mute_profile.mute_ramp_off_delay_per_step_us);
--- sw/src/mutehandling.hpp
+++ sw/src/mutehandling.hpp
@@ -13,10 +13,16 @@ struct DeviceSpecificMuteProfile {
     bool extra_drive_opto;
 };
 
+static constexpr AD5258::Value dampeningToAD5258WiperPos(const MuteProfile::Dampening dampening) {
+    // Invert and scale to max val
+    // FIXME: Float is ugly. How to do completely in integer?
+    return (MuteProfile::max_dampening - dampening) * ((1.0 * AD5258::max_wiper_val) / MuteProfile::max_dampening);
+}
+
 static constexpr DeviceSpecificMuteProfile calculateValuesFromMuteProfile(const MuteProfile& profile){
     // Assumption: High value -> High resistance -> Low optocoupler -> low dampening -> microphone "on"
-    AD5258::Value value_when_microphone_off = (MuteProfile::max_dampening - profile.dampening_when_microphone_off) * AD5258::max_wiper_val;
-    AD5258::Value value_when_microphone_on = (MuteProfile::max_dampening - profile.dampening_when_microphone_on) * AD5258::max_wiper_val;
+    AD5258::Value value_when_microphone_off = dampeningToAD5258WiperPos(profile.dampening_when_microphone_off);
+    AD5258::Value value_when_microphone_on = dampeningToAD5258WiperPos(profile.dampening_when_microphone_on);
     AD5258::Value on_off_diff = abs(value_when_microphone_off - value_when_microphone_on);
     bool extra_drive_opto = profile.dampening_when_microphone_off == MuteProfile::max_dampening;
 
@@ -24,6 +30,15 @@ static constexpr DeviceSpecificMuteProfile calculateValuesFromMuteProfile(const 
                         (profile.mute_ramp_on_time_ms * 1000) / on_off_diff;
     DeviceSpecificMuteProfile::Time_us mute_ramp_off_delay_per_step_us =
                         (profile.mute_ramp_off_time_ms * 1000) / on_off_diff;
+
+    // error: non-constant condition for static assertion
+    // error: the value of 'value_when_microphone_on' is not usable in a constant expression
+    /*
+    static_assert(value_when_microphone_on < AD5258::max_wiper_val,
+                  "value_when_microphone_on may not be bigger than max wiper val");
+    static_assert(value_when_microphone_off < AD5258::max_wiper_val,
+                  "value_when_microphone_off may not be bigger than max wiper val");
+    */
 
     return DeviceSpecificMuteProfile {
         .mute_ramp_on_delay_per_step_us = mute_ramp_on_delay_per_step_us,
